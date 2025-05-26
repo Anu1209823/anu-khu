e3d5201dcb6b8c7afc62fdbeffc3fcc38705983b
Anushika chauhan <anuhsikachauhananu@yahoo.com>
2025-05-26 20:09:03 +1000

    Fixed topic names for anukhu

--- packages/open_loop_square/src/open_loop_square.py
+++ packages/open_loop_square/src/open_loop_square.py
@@ -13,8 +13,8 @@ class Drive_Square:
         rospy.init_node('drive_square_node', anonymous=True)
         
         #Initialize Pub/Subs
-        self.pub = rospy.Publisher('/akandb/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
-        rospy.Subscriber('/akandb/fsm_node/mode', FSMState, self.fsm_callback, queue_size=1)
+        self.pub = rospy.Publisher('/anukhu/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
+        rospy.Subscriber('/anukhu/fsm_node/mode', FSMState, self.fsm_callback, queue_size=1)
         
     # robot only moves when lane following is selected on the duckiebot joystick app
     def fsm_callback(self, msg):
@@ -29,7 +29,7 @@ class Drive_Square:
     def stop_robot(self):
         self.cmd_msg.header.stamp = rospy.Time.now()
         self.cmd_msg.v = 0.0
-        self.cmd_msg.omega = 0.0
+        self.cmd_msg.omega = 3.14
         self.pub.publish(self.cmd_msg)
  
     # Spin forever but listen to message callbacks
