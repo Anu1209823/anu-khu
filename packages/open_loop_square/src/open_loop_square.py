b4fc11f6562e67c7adb2dbcbfca2db151ef72b50
Anushika chauhan <anuhsikachauhananu@yahoo.com>
2025-05-26 20:23:40 +1000

    gfdg

--- packages/open_loop_square/src/open_loop_square.py
+++ packages/open_loop_square/src/open_loop_square.py
@@ -1,62 +1,70 @@
 #!/usr/bin/env python3
 
 import rospy
-from duckietown_msgs.msg import Twist2DStamped
-from duckietown_msgs.msg import FSMState
- 
+from duckietown_msgs.msg import Twist2DStamped, FSMState
+
 class Drive_Square:
     def __init__(self):
-        #Initialize global class variables
+        # Initialize Twist message
         self.cmd_msg = Twist2DStamped()
 
-        #Initialize ROS node
+        # Start ROS node
         rospy.init_node('drive_square_node', anonymous=True)
-        
-        #Initialize Pub/Subs
-        self.pub = rospy.Publisher('/anukhu/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
-        rospy.Subscriber('/anukhu/fsm_node/mode', FSMState, self.fsm_callback, queue_size=1)
-        
-    # robot only moves when lane following is selected on the duckiebot joystick app
+
+        # Update this to your actual Duckiebot name
+        self.vehicle_name = "anukhupi"
+
+        # Publisher to send motion commands
+        self.pub = rospy.Publisher(
+            f"/{self.vehicle_name}/car_cmd_switch_node/cmd", 
+            Twist2DStamped, 
+            queue_size=1
+        )
+
+        # Subscriber to listen for FSM mode
+        rospy.Subscriber(
+            f"/{self.vehicle_name}/fsm_node/mode", 
+            FSMState, 
+            self.fsm_callback, 
+            queue_size=1
+        )
+
     def fsm_callback(self, msg):
-        rospy.loginfo("State: %s", msg.state)
-        if msg.state == "NORMAL_JOYSTICK_CONTROL":
-            self.stop_robot()
-        elif msg.state == "LANE_FOLLOWING":            
-            rospy.sleep(1) # Wait for a sec for the node to be ready
+        rospy.loginfo(f"FSM mode received: {msg.state}")
+        if msg.state == "NORMAL_LANE_FOLLOWING":
+            rospy.sleep(1)
             self.move_robot()
- 
-    # Sends zero velocities to stop the robot
+
     def stop_robot(self):
         self.cmd_msg.header.stamp = rospy.Time.now()
         self.cmd_msg.v = 0.0
-        self.cmd_msg.omega = 3.14
+        self.cmd_msg.omega = 0.0
         self.pub.publish(self.cmd_msg)
- 
-    # Spin forever but listen to message callbacks
-    def run(self):
-    	rospy.spin() # keeps node from exiting until node has shutdown
 
-    # Robot drives in a square and then stops
     def move_robot(self):
+        for i in range(4):  # 4 sides of a square
+            # Drive forward
+            self.cmd_msg.header.stamp = rospy.Time.now()
+            self.cmd_msg.v = 0.3
+            self.cmd_msg.omega = 0.0
+            self.pub.publish(self.cmd_msg)
+            rospy.loginfo(f"Side {i+1}: Driving forward")
+            rospy.sleep(1.5)
 
-        #YOUR CODE GOES HERE#
-        self.cmd_msg.header.stamp = rospy.Time.now()
-        self.cmd_msg.v = 0.5 # striaght line velocity
-        self.cmd_msg.omega = 0.0
-        self.pub.publish(self.cmd_msg)
-        rospy.loginfo("Forward!")
-        rospy.sleep(1) # straight line driving time
-        
-        self.cmd_msg.header.stamp = rospy.Time.now()
-        self.cmd_msg.v = -0.5 # striaght line velocity
-        self.cmd_msg.omega = 0.0
-        self.pub.publish(self.cmd_msg)
-        rospy.loginfo("Backward!")
-        rospy.sleep(1) # straight line driving time
-        
-        ######################
-                
+            # Turn 90 degrees
+            self.cmd_msg.header.stamp = rospy.Time.now()
+            self.cmd_msg.v = 0.0
+            self.cmd_msg.omega = 3.14  # ~180 deg/sec
+            self.pub.publish(self.cmd_msg)
+            rospy.loginfo(f"Side {i+1}: Turning")
+            rospy.sleep(0.5)  # ~90 degrees turn
+
         self.stop_robot()
+        rospy.loginfo("Square completed. Robot stopped.")
+
+    def run(self):
+        rospy.spin()
+
 
 if __name__ == '__main__':
     try:
@@ -64,3 +72,4 @@ if __name__ == '__main__':
         duckiebot_movement.run()
     except rospy.ROSInterruptException:
         pass
+
