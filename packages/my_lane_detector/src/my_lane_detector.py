ca1b0e37d67ca42a6f531b2b9eb7bc70ec5050a9
Anushika chauhan <anuhsikachauhananu@yahoo.com>
2025-05-31 00:42:12 +1000

    hsvfch

--- packages/my_lane_detector/src/my_lane_detector.py
+++ packages/my_lane_detector/src/my_lane_detector.py
@@ -32,10 +32,23 @@ class Lane_Detector:
     def image_callback(self, msg):
         img = self.cv_bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
         height, width, _ = img.shape
-        cropped = img[int(height/2):, :]
+        cropped = img[int(height*0.4):, :]   # Try with more of the image for debugging
         hsv = cv2.cvtColor(cropped, cv2.COLOR_BGR2HSV)
-        cv2.imshow('HSV', hsv)
+
+        # Tune your white threshold here
+        lower_white = np.array([0, 0, 120])
+        upper_white = np.array([180, 70, 255])
+        white_mask = cv2.inRange(hsv, lower_white, upper_white)
+        cv2.imshow('White Mask', white_mask)
+
+        # Tune your yellow threshold here
+        lower_yellow = np.array([10, 40, 40])
+        upper_yellow = np.array([40, 255, 255])
+        yellow_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        cv2.imshow('Yellow Mask', yellow_mask)
+
         cv2.waitKey(1)
+
 
 
     def run(self):
